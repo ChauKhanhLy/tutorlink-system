b435bd6076d50a25b8fe28ad3d2b98670b7c48d5
tn26205 <23020717@vnu.edu.vn>
2026-04-14 02:23:15 +0700

    Protected API (/users/me)

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -2,15 +2,20 @@ const jwt = require('jsonwebtoken')
 const SECRET = process.env.JWT_SECRET
 
 module.exports = (req, res, next) => {
-  const token = req.headers.authorization
+  const authHeader = req.headers.authorization   // lấy header
 
-  if (!token) {
+  if (!authHeader) {
     return res.status(401).json({ message: "No token" })
   }
 
   try {
+    //  tách "Bearer <token>"
+    const token = authHeader.split(' ')[1]
+
     const decoded = jwt.verify(token, SECRET)
+
     req.user = decoded
+
     next()
   } catch (err) {
     return res.status(403).json({ message: "Invalid token" })
