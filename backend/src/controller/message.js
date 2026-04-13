5f651196c51eeba2d51e95ca1fb4ba0a74b6fd46
DoThiBichNgoc21 <161515353+DoThiBichNgoc21@users.noreply.github.com>
2026-04-13 16:33:49 +0700

    update api

--- backend/src/controller/message.js
+++ backend/src/controller/message.js
@@ -1,4 +1,5 @@
 //import pool from '../../config/database.js';
+import { v4 as uuidv4 } from 'uuid';
 import { Op } from 'sequelize';
 import Message from '../models/message.model.js';
 
@@ -29,6 +30,31 @@ export const getMessagesBetweenUsers = async (req, res) => {
     });
 
     res.status(200).json(messages);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const createMessage = async (req, res) => {
+  try {
+    const { sender_id, receiver_id, content } = req.body;
+
+    if (!sender_id || !receiver_id || !content) {
+      return res.status(400).json({
+        error: 'Thiếu sender_id, receiver_id hoặc content',
+      });
+    }
+
+    const message = await Message.create({
+      id: uuidv4(),
+      sender_id,
+      receiver_id,
+      content,
+      sent_at: new Date(),
+      is_read: false,
+    });
+
+    res.status(201).json(message);
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
