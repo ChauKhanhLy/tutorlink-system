f98c024e6802fb4dc7c57bfce2bdefba41570c23
DoThiBichNgoc21 <161515353+DoThiBichNgoc21@users.noreply.github.com>
2026-05-31 18:20:04 +0700

    fix_avatar

--- backend/src/app.js
+++ backend/src/app.js
@@ -40,8 +40,7 @@ app.use(cors({
 
 app.use(express.json())
 
-//app.use("/uploads", express.static("uploads"));
-//app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 app.use('/api/auth', authRoutes)
 app.use('/api/tutors', tutorRoutes)
 app.use('/api/matching', matchingRoutes)
